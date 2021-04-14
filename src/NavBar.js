d3b6fda24924f30352b95a22bbfb335acf0ea465
PabloDM <phdemarco@gmail.com>
2021-04-13 21:56:03 -0300

    cambios scss

--- src/NavBar.js
+++ src/NavBar.js
@@ -7,7 +7,7 @@ function NavBar() {
         
 <nav className="navBar">
     <div class="nav-wrapper">
-    <a className="nombre_logo" href="/#" > insumos <strong>medicos</strong></a>
+      <a className="nombre_logo" href="/#" > insumos <strong>medicos</strong></a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a href="/#">carrito</a></li>
         <li><a href="/#">productos</a></li>
